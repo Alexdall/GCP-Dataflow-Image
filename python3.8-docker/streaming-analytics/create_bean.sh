c0442b8484a1e8a861a4461bbc2fa6dc05906f5b
Alexandre <alexandredall@yahoo.com.br>
2022-09-28 23:48:39 -0300

    Update create_bean.sh

--- python3.8-docker/streaming-analytics/create_bean.sh
+++ python3.8-docker/streaming-analytics/create_bean.sh
@@ -2,7 +2,7 @@
 
 python3.8 -m pip install --upgrade pip
 python3.8 -m pip install --no-cache-dir -r requirements.txt
-export GOOGLE_APLLICATION_CREDENTIALS="global-sign-361417-edbd1632b67b.json" 
+export GOOGLE_APPLICATION_CREDENTIALS="global-sign-361417-edbd1632b67b.json" 
 
 python3.8 PubSubToGCS.py \
 	  --project=$PROJECT_ID \
